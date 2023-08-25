de1814c2c09ed3c9b95e10d7bcc17c0266e7d04a
Akash <ag308669@gmail.com>
2023-08-25 18:52:31 +0530

    Minor modifications

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Container from '@/app/components/Container';
+import ListingCard from '@/app/components/listings/ListingCard';
+import EmptyState from '@/app/components/EmptyState';
+
+import getListings, { IListingsParams } from '@/app/actions/getListings';
+import getCurrentUser from '@/app/actions/getCurrentUser';
 import ClientOnly from './components/ClientOnly';
-import Container from './components/Container';
-import EmptyState from './components/EmptyState';
-import getCurrentUser from './actions/getCurrentUser';
-import getListings, { IListingsParams } from './actions/getListings';
-import ListingCard from './components/listings/ListingCard';
 
 interface HomeProps {
   searchParams: IListingsParams;
@@ -27,8 +28,8 @@ const Home = async ({ searchParams }: HomeProps) => {
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {listings.map((listing: any) => (
             <ListingCard
-              key={listing.id}
               currentUser={currentUser}
+              key={listing.id}
               data={listing}
             />
           ))}
